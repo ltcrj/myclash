7ca85b07b3e415f46a090fc94a87c2264aaaef12
绿野辰星 <lt@users.noreply.github.com>
2024-11-06 11:06:54 +0800

    20241106-new

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
-from urllib.parse import urljoin
 import requests
 from lxml import etree
 
+url = r'https://clashgithub.com/'
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36'
+}
 
-
-url = r'https://free.datiya.com'
-res = requests.get(url)
+res = requests.get(url=url, headers=headers)
 et = etree.HTML(res.text)
-tail_url = et.xpath('//section[1]/div[1]//h1/a/@href')[0] or None
+detail_urls = et.xpath('//*[@id="posts"]/div[1]//h3/a/@href')[0] or None
+print(detail_urls)
 
-if tail_url:
-    full_url = urljoin(url, tail_url)
-    sub_res = requests.get(full_url)
-    sub_et = etree.HTML(sub_res.text)
-    sub_url = sub_et.xpath('//pre[1]/code/text()')[0].strip() or None
-    if sub_url:
-        result = requests.get(sub_url)
+if detail_urls:
+    detail_res = requests.get(url=detail_urls, headers=headers)
+    detail_et = etree.HTML(detail_res.text)
+    final_url = detail_et.xpath('//p[strong="clash订阅链接:"]/following-sibling::p[1]/text()')[0] or None
+    if final_url:
+        clash_detail = requests.get(url=final_url, headers=headers)
         with open('./tmp.yaml', 'wb') as f:
-            f.write(result.content)
+            f.write(clash_detail.content)
     else:
         with open('./tmp.yaml', 'w', encoding='utf-8') as f:
             f.write('error: do not get any information')
